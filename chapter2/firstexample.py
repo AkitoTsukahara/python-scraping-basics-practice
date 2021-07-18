94d0efbf5e053ec2b973f53f9069d4b58e911fed
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-07-18 22:43:49 +0900

    requestメソッド

--- chapter2/firstexample.py
+++ chapter2/firstexample.py
@@ -3,3 +3,15 @@ import requests
 url = 'http://www.webscrapingfordatascience.com/basichttp/'
 r = requests.get(url)
 print(r.text)
+# サーバーから返却されたHTTpステータスコード
+print(r.status_code)
+# テキストのステータスメッセージ
+print(r.reason)
+# HTTPレスポンスヘッダー
+print(r.headers)
+# リクエスト情報はr.rquestにPythonオブジェクトとして保存される
+print(r.request)
+# HTTPリクエストヘッダーは？
+print(r.request.headers)
+# HTTPレスポンスのコンテンツ
+print(r.text)
