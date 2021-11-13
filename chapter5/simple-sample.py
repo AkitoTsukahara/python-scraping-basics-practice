76e23bdefcb79b5830269c0494e825c2d15225e3
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-11-13 22:47:10 +0900

    lesson chapter5

---
+++ chapter5/simple-sample.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+url = 'http://www.webscrapingfordatascience.com/complexjavascript/'
+driver = webdriver.Chrome()
+driver.get(url)
+
+input('Press ENTER to close the automated browser')
